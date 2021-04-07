ffe3d3db5a41c28a78ec4dabf0fdd87524b0f333
tavkhelidzeluka <Tavkhelidzeluka@gmail.com>
2021-04-07 22:30:54 +0400

    finish homework 1

--- Algorithms/Homework_1.h
+++ Algorithms/Homework_1.h
@@ -7,7 +7,11 @@
 
 namespace graph_homework_1 {
 	/*
-		Lab 3 homework N1
+		Lab 3 Tasks :
+		N1
+		N2
+		N3
+		N4
 	*/
 	class Graph
 	{
@@ -29,11 +33,19 @@ namespace graph_homework_1 {
 			for (const auto& vertex_name : vertex_names)
 				this->adjacency_list[vertex_name] = std::vector<char>{};
 		}
-		void add_edge(char from, char to)
+		void add_edge(char from, char to, bool oriented = true)
 		{
 			for (const auto& vertex : this->adjacency_list[from])
 				if (vertex == to)
 					return;
+
+			if (not oriented)
+			{
+				for (const auto& vertex : this->adjacency_list[to])
+					if (vertex == from)
+						return;
+				adjacency_list[to].push_back(from);
+			}
 
 			adjacency_list[from].push_back(to);
 		}
@@ -54,17 +66,23 @@ namespace graph_homework_1 {
 			return output;
 		}
 
-		void bfs(char start)
+		void bfs(char start, int homework = 1)
 		{
 			std::queue<char> active_vertecies;
 			std::map<char, bool> visited;
+			std::map<char, int> depth;
+
 			char current_vertex;
 
-			for (const auto& [key, value] : visited)
-				visited[key] = false;
+			for (const auto& [vertex, _] : this->adjacency_list) 
+			{
+				visited[vertex] = false;
+				depth[vertex] = 9999999;
+			}
 
 			active_vertecies.push(start);
 			visited[start] = true;
+			depth[start] = 0;
 
 			while (not active_vertecies.empty())
 			{
@@ -75,15 +93,104 @@ namespace graph_homework_1 {
 				{
 					if (not visited[v])
 					{
+						depth[v] = depth[current_vertex] + 1;
 						visited[v] = true;
 						active_vertecies.push(v);
 					}
 				}
 			}
 
-			std::cout << "visited vertecies: ";
-			for (const auto& [vertex, _] : visited)
-				std::cout << vertex << " | ";
+			if (homework == 1)
+			{
+				std::cout << "visited vertecies: ";
+				for (const auto& [vertex, _] : visited)
+					std::cout << vertex << " | ";
+			}
+			else if (homework == 2)
+			{
+				unsigned count = 0;
+				std::cout << "vertecies that can't be reached: ";
+				for (const auto& [vertex, _] : this->adjacency_list) 
+				{
+					if (not visited[vertex])
+					{
+						count++;
+						std::cout << vertex << " | ";
+					}
+				}
+
+				if (count == 0)
+					std::cout << "There are no such vertecies";
+			}
+			else if (homework == 3)
+			{
+				unsigned count = 0;
+				std::cout << "vertecies that are 2 depth far from starting vertex: ";
+				for (const auto& [vertex, _] : visited)
+				{
+					if (depth[vertex] == 2)
+					{
+						count++;
+						std::cout << vertex << " | ";
+					}
+				}
+				if (count == 0)
+					std::cout << "There are no such vertecies";
+			}
+
+			std::cout << std::endl;
+		}
+
+		void bfs_task_4()
+		{
+			std::queue<char> active_vertecies;
+			std::map<char, bool> visited;
+			std::map<char, bool> finished;
+			std::map<char, bool> colored;
+
+			char current_vertex;
+
+			for (const auto& [vertex, _] : this->adjacency_list)
+			{
+				visited[vertex] = false;
+				finished[vertex] = false;
+				colored[vertex] = false;
+			}
+
+			for (const auto& [start, _] : this->adjacency_list)
+			{
+				if (not visited[start]) 
+				{
+					active_vertecies.push(start);
+					visited[start] = true;
+					colored[start] = true;
+
+					while (not active_vertecies.empty())
+					{
+						current_vertex = active_vertecies.front();
+						active_vertecies.pop();
+
+						for (const auto& v : adjacency_list[current_vertex])
+						{
+							if (not visited[v])
+							{
+								colored[v] = not colored[current_vertex];
+								visited[v] = true;
+								active_vertecies.push(v);
+							}
+							else if (not finished[v] and colored[v] == colored[current_vertex])
+							{
+								std::cout << "Graph can't be colored in 2 colors" << std::endl;
+								return;
+							}
+						}
+						finished[current_vertex] = true;
+					}
+				}
+			}
+
+			for (const auto& [vertex, color] : colored)
+				std::cout << vertex << " " << color << ", ";
 		}
 
 		friend std::ostream& operator <<(std::ostream& output, Graph& object)
@@ -94,5 +201,43 @@ namespace graph_homework_1 {
 
 	};
 
+	void test_homework_1()
+	{
+		Graph b({ 'b', 'c', 'd', 'f' });
+
+		b.add_edge('b', 'a');
+		b.add_edge('a', 'd');
+		b.add_edge('c', 'b');
+		b.add_edge('d', 'b');
+		b.add_edge('f', 'a');
+		b.add_edge('f', 'c');
+
+		std::cout << b << std::endl;
+
+		// N1
+		b.bfs('a');
+		std::cout << std::endl;
+
+		// N2
+		b.bfs('d', 2);
+		std::cout << std::endl;
+
+		// N3
+		b.bfs('a', 3);
+		std::cout << std::endl;
+
+		// N4
+		Graph color_it(6);
+
+		color_it.add_edge('a', 'c', false);
+		color_it.add_edge('a', 'd', false);
+		color_it.add_edge('c', 'b', false);
+		color_it.add_edge('b', 'd', false);	
+		color_it.add_edge('f', 'd', false);
+
+		std::cout << color_it << std::endl;
+
+		color_it.bfs_task_4();
+	}
 }
 
--- Algorithms/main.cpp
+++ Algorithms/main.cpp
@@ -2,22 +2,10 @@
 
 using namespace std;
 using Collection::List;
-using graph_homework_1::Graph;
 
 
 int main()
 {
-	Graph b({ 'b', 'c', 'd', 'f' });
-
-	b.add_edge('b', 'a');
-	b.add_edge('a', 'd');
-	b.add_edge('c', 'b');
-	b.add_edge('d', 'b');
-	b.add_edge('f', 'a');
-	b.add_edge('f', 'c');
-
-	cout << b << endl;
-
-	b.bfs('a');
+	graph_homework_1::test_homework_1();
 	return EXIT_SUCCESS;
 }
